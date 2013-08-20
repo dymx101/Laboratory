a14351a9bbb4a0ad1b52546886619f950fcee481
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-08-20 18:10:53 +0800

    update

--- TestEffect/TestEffect/TMDViewController.m
+++ TestEffect/TestEffect/TMDViewController.m
@@ -26,11 +26,20 @@
     [blurView addGestureRecognizer:panGest];
 }
 
--(void)pan:(UIPanGestureRecognizer *)gest
-{
-    CGPoint pt = [gest translationInView:gest.view];
-    UIView *view = gest.view;
-    view.frame = CGRectMake(pt.x, pt.y, view.frame.size.width, view.frame.size.height);
+//-(void)pan:(UIPanGestureRecognizer *)gest
+//{
+//    CGPoint pt = [gest translationInView:gest.view];
+//    UIView *view = gest.view;
+//    view.frame = CGRectMake(pt.x, pt.y, view.frame.size.width, view.frame.size.height);
+//}
+
+- (void)pan:(UIPanGestureRecognizer *)gesture {
+    if ((gesture.state == UIGestureRecognizerStateChanged) ||
+        (gesture.state == UIGestureRecognizerStateEnded)) {
+        CGPoint translation = [gesture translationInView:self.view];
+        gesture.view.center = CGPointMake(gesture.view.center.x + translation.x, gesture.view.center.y + translation.y);
+        [gesture setTranslation:CGPointZero inView:self.view];
+    }
 }
 
 - (void)didReceiveMemoryWarning
