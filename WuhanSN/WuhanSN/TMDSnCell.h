6a7ae22e0213c71865f250857813339e99f2a942
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-10-13 12:05:31 +0800

    working

---
+++ WuhanSN/WuhanSN/TMDSnCell.h
@@ -0,0 +1,13 @@
+//
+//  TMDSnCell.h
+//  WuhanSN
+//
+//  Created by Dong Yiming on 10/13/13.
+//  Copyright (c) 2013 ToMaDon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMDSnCell : UITableViewCell
+
+@end
