3c951a9e3c5bb0f130c71e6d7d8ad50269c36222
Daniel Dong <ydong@gagein.com>
2013-10-23 17:16:09 +0800

    add appframework

---
+++ AppFramework/AppFramework/DDAppDelegate.m
@@ -0,0 +1,82 @@
+//
+//  DDAppDelegate.m
+//  AppFramework
+//
+//  Created by Dong Yiming on 10/23/13.
+//  Copyright (c) 2013 ToMaDon. All rights reserved.
+//
+
+#import "DDAppDelegate.h"
+
+#import "DDViewController.h"
+#import "MMDrawerController.h"
+
+@implementation DDAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    //self.viewController = [[DDViewController alloc] initWithNibName:@"DDViewController" bundle:nil];
+    
+    
+    UIViewController * leftDrawer = [[DDTestLeftVC alloc] init];
+    UIViewController * center = [[DDTestCenterVC alloc] init];
+    UIViewController * rightDrawer = [[DDTestRightVC alloc] init];
+    
+    MMDrawerController * drawerController = [[MMDrawerController alloc]
+                                             initWithCenterViewController:center
+                                             leftDrawerViewController:leftDrawer
+                                             rightDrawerViewController:rightDrawer];
+    
+    [drawerController setRestorationIdentifier:@"MMDrawer"];
+    [drawerController setMaximumRightDrawerWidth:100.0];
+    [drawerController setOpenDrawerGestureModeMask:MMOpenDrawerGestureModeAll];
+    [drawerController setCloseDrawerGestureModeMask:MMCloseDrawerGestureModeAll];
+    
+//    [drawerController
+//     setDrawerVisualStateBlock:^(MMDrawerController *drawerController, MMDrawerSide drawerSide, CGFloat percentVisible) {
+//         MMDrawerControllerDrawerVisualStateBlock block;
+//         block = [[MMExampleDrawerVisualStateManager sharedManager]
+//                  drawerVisualStateBlockForDrawerSide:drawerSide];
+//         if(block){
+//             block(drawerController, drawerSide, percentVisible);
+//         }
+//     }];
+
+    
+    
+    self.window.rootViewController = drawerController;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ AppFramework/AppFramework/ViewControllers/DDViewController.m
@@ -0,0 +1,57 @@
+//
+//  DDViewController.m
+//  AppFramework
+//
+//  Created by Dong Yiming on 10/23/13.
+//  Copyright (c) 2013 ToMaDon. All rights reserved.
+//
+
+#import "DDViewController.h"
+
+@interface DDViewController ()
+
+@end
+
+@implementation DDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    //self.view.backgroundColor = [UIColor colorWithCrayola:@""];
+}
+
+@end
+
+@implementation DDTestLeftVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    self.view.backgroundColor = [UIColor colorWithCrayola:@"Caribbean Green"];
+}
+
+@end
+
+@implementation DDTestCenterVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    self.view.backgroundColor = [UIColor colorWithCrayola:@"Mango Tango"];
+}
+
+@end
+
+@implementation DDTestRightVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    self.view.backgroundColor = [UIColor colorWithCrayola:@"Midnight Blue"];
+}
+
+@end
